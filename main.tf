278f1d149211fe8ed5ea63e85cd267d306e1d187
“MEERIM <meerim.us11@gmail.com>
2024-03-14 16:42:05 -0700

    terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,25 @@
+# The AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.35.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
+}
+
+terraform {
+  cloud {
+    organization = "meerim_omurbek"
+
+    workspaces {
+      name = "First_project"
+    }
+  }
 }
 
 resource "aws_ecr_repository" "ecr_meerim" {
